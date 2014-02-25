7f64b3e22f2eb343a05e0c4b1f1fd1d49bf7e379
Tom Leo <tom@tomleo.com>
2014-02-25 10:39:40 -0500

    Added DjagnoRestFramework

--- moosic/urls.py
+++ moosic/urls.py
@@ -3,10 +3,24 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets, routers
+
+class UserViewSet(viewsets.ModelViewSet):
+    model = User
+
+class GroupViewSet(viewsets.ModelViewSet):
+    model = Group
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'groups', GroupViewSet)
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'moosic.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
