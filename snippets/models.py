f4f58647b3b4b2d8e77c183bbaef3b93a44809a1
Tom Leo <tom@tomleo.com>
2014-02-25 14:05:31 -0500

    Fixed typo

--- snippets/models.py
+++ snippets/models.py
@@ -4,7 +4,7 @@ from pygments.styles import get_all_styles
 
 LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
-STYLE_CHOCIES = sorted((item, item) for item in get_all_styles())
+STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
 
 class Snippet(models.Model):
     created = models.DateTimeField(auto_now_add=True)
