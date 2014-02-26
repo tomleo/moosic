b23d4ffffe67d0b2e17bcbf3cfdf81de5430525e
Tom Leo <tom@tomleo.com>
2014-02-26 18:03:29 -0500

    made model field singular

--- snippets/models.py
+++ snippets/models.py
@@ -11,7 +11,7 @@ class Snippet(models.Model):
     title = models.CharField(max_length=100, blank=True, default='')
     code = models.TextField()
     linenos = models.BooleanField(default=False)
-    languages = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
+    language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
     style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
 
     class Meta:
