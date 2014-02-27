9752aeb5b1e2a566d76e39b5c0e4637db76f926b
Tom Leo <tom@tomleo.com>
2014-02-27 11:28:48 -0500

    Fixed typo

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 
-urlpattern = patterns('snippets.views',
+urlpatterns = patterns('snippets.views',
     url(r'^snippets/$', 'snippet_list'),
     url(r'^snippets/(?P<pk>[0-9]+)/$', 'snippet_detail')
 )
